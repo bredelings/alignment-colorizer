9b782d69e62ddd2260d298b3bd1d79715e13a003
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-12-10 15:14:36 -0500

    Read some arguments directly

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 mod fasta_parser;
 
+struct Cli {
+    alignment_fn: std::path::PathBuf,
+    property_fn: std::path::PathBuf
+}
+
 fn main() {
     
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+    let alignment_fn = std::env::args().nth(1).expect("no pattern given");
+    let property_fn = std::env::args().nth(2).expect("no path given");
+
+    let args = Cli {
+        alignment_fn: std::path::PathBuf::from(alignment_fn),
+        property_fn: std::path::PathBuf::from(property_fn)
+    };
+    
+    println!("args: alignment_fn = {:?} property_fn = {:?}", args.alignment_fn, args.property_fn);
 
     let fa = fasta_parser::parse_alignment();
-    println!("Hello, world!  pattern: {:?} path: {:?}", pattern, path);
     println!("{:?}", fa);
 }
