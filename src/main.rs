80056b9dd52a4f77481d609f2ae1d66ea2fcea68
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-12-10 15:21:11 -0500

    Use clap to derive a cmdline options parser

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 mod fasta_parser;
 
+use clap::Parser;
+
+#[derive(Parser)]
 struct Cli {
+    /// The alignment filename
     alignment_fn: std::path::PathBuf,
+    /// The properties filename
     property_fn: std::path::PathBuf
 }
 
 fn main() {
+    let args = Cli::parse();
     
-    let alignment_fn = std::env::args().nth(1).expect("no pattern given");
-    let property_fn = std::env::args().nth(2).expect("no path given");
-
-    let args = Cli {
-        alignment_fn: std::path::PathBuf::from(alignment_fn),
-        property_fn: std::path::PathBuf::from(property_fn)
-    };
-    
-    println!("args: alignment_fn = {:?} property_fn = {:?}", args.alignment_fn, args.property_fn);
+    println!("alignment_fn: {:?} property_fn: {:?}", args.alignment_fn, args.property_fn);
 
     let fa = fasta_parser::parse_alignment();
     println!("{:?}", fa);
