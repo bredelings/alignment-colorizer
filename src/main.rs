200b1fee93f02ce1de7eaaa761f2198e4ab85ff2
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-12-11 11:30:21 -0500

    Try using ? instead

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,15 @@ struct Cli {
     property: String
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     
     println!("alignment_fn: {:?} property_fn: {:?}", args.alignment_fn, args.property_fn);
 
-    let alignment_result = std::fs::read_to_string(&args.alignment_fn);
-    let alignment_content = match alignment_result {
-        Ok(content) => { content },
-        Err(error) => { panic!("Can't deal with {error}, just exit here") }
-    };
-            
-    let _properties_content = std::fs::read_to_string(&args.property_fn).expect("could not read properties file");
+    let alignment_content = std::fs::read_to_string(&args.alignment_fn)?;
+    let _properties_content = std::fs::read_to_string(&args.property_fn)?;
 
     let fa = fasta_parser::parse_alignment(alignment_content);
     println!("{:?}", fa);
+    Ok(())
 }
