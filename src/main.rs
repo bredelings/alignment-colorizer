32758bf90c05b0981428730180db74fb5ce90cfe
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-12-11 11:31:04 -0500

    Use library anyhow for better error message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod fasta_parser;
 
+use anyhow::{Context, Result};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -12,13 +13,16 @@ struct Cli {
     property: String
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    
+
     println!("alignment_fn: {:?} property_fn: {:?}", args.alignment_fn, args.property_fn);
 
-    let alignment_content = std::fs::read_to_string(&args.alignment_fn)?;
-    let _properties_content = std::fs::read_to_string(&args.property_fn)?;
+    let alignment_content = std::fs::read_to_string(&args.alignment_fn)
+        .with_context(|| format!("could not read file `{:?}`",args.alignment_fn))?;
+    let _properties_content = std::fs::read_to_string(&args.property_fn)
+        .with_context(|| format!("could not read file `{:?}`",args.property_fn))?;
+    let _property = args.property;
 
     let fa = fasta_parser::parse_alignment(alignment_content);
     println!("{:?}", fa);
