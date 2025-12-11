b8f0652e2ded05e4a9efb94985d8fd9bab264aa2
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-12-11 17:33:16 -0500

    Use .enumerate() instead of a mutable index

--- src/fasta_parser.rs
+++ src/fasta_parser.rs
@@ -10,12 +10,10 @@ pub fn parse_alignment(content: String) -> Vec<FastaRecord> {
     let n_lines = lines.count();
     let pb = indicatif::ProgressBar::new(n_lines.try_into().unwrap());
 
-    let mut i=0;
-    for _line in content.lines() {
+    for (index,_line) in content.lines().enumerate() {
         std::thread::sleep(std::time::Duration::from_millis(10));
-        pb.println(format!("[+] finished #{i}"));
+        pb.println(format!("[+] finished #{index}"));
         pb.inc(1);
-        i += 1;
     }
 
     let rec1 = FastaRecord {name: "Homo".to_string(), sequence: "ATGC".to_string()};
