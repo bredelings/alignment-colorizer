976ca24f200087c1c341b99aa7d0c75125d3ae6e
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-12-11 11:22:18 -0500

    Match on the result

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ struct Cli {
     /// The alignment filename
     alignment_fn: std::path::PathBuf,
     /// The properties filename
-    property_fn: std::path::PathBuf
+    property_fn: std::path::PathBuf,
+    /// The property name
+    property: String
 }
 
 fn main() {
@@ -15,7 +17,12 @@ fn main() {
     
     println!("alignment_fn: {:?} property_fn: {:?}", args.alignment_fn, args.property_fn);
 
-    let alignment_content = std::fs::read_to_string(&args.alignment_fn).expect("could not read alignment file");
+    let alignment_result = std::fs::read_to_string(&args.alignment_fn);
+    let alignment_content = match alignment_result {
+        Ok(content) => { content },
+        Err(error) => { panic!("Can't deal with {error}, just exit here") }
+    };
+            
     let _properties_content = std::fs::read_to_string(&args.property_fn).expect("could not read properties file");
 
     let fa = fasta_parser::parse_alignment(alignment_content);
