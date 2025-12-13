cc29d34d70d85081e1bf994cf1dc4efccfc658d1
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-12-13 13:15:28 -0500

    Extract the property from the JSON

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod fasta_parser;
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use clap::Parser;
 use log::info;
+use serde_json;
 
 #[derive(Parser)]
 struct Cli {
@@ -13,30 +14,47 @@ struct Cli {
     alignment_fn: std::path::PathBuf
 }
 
+fn get_alignment(filename: &std::path::Path) -> Result<Vec<fasta_parser::FastaRecord>, anyhow::Error>
+{
+    let alignment_content = std::fs::read_to_string(filename)
+        .with_context(|| format!("could not read file `{:?}`",filename))?;
+    info!("Read alignment");
+    let sequences = fasta_parser::parse_alignment(alignment_content);
+    info!("Got {:?} sequences.", sequences.len());
+    
+    Ok(sequences)
+}
 
-fn main() -> Result<()> {
 
+
+fn get_property_values(filename: &std::path::Path, property: &str) -> Result<serde_json::Value>
+{
+    let properties_content = std::fs::read_to_string(filename)
+        .with_context(|| format!("could not read file `{:?}`",filename))?;
+    info!("Read properties file");
+    let properties_json: serde_json::Value = serde_json::from_str(&properties_content)
+        .with_context(|| format!("failed to parse JSON from file `{}`", filename.display()))?;
+
+    properties_json.get(property)
+        .ok_or_else(|| anyhow!("Properties JSON has no property '{}'",property))
+        .map(|json| json.clone())
+}
+
+fn main() -> Result<()>
+{
     env_logger::init();
+
     let args = Cli::parse();
 
-    println!("alignment_fn: {:?} property_fn: {:?}", args.alignment_fn, args.property_fn);
+    let sequences = get_alignment(&args.alignment_fn)?;
 
-    let alignment_content = std::fs::read_to_string(&args.alignment_fn)
-        .with_context(|| format!("could not read file `{:?}`",args.alignment_fn))?;
-    info!("Read alignment");
-    let _properties_content = std::fs::read_to_string(&args.property_fn)
-        .with_context(|| format!("could not read file `{:?}`",args.property_fn))?;
-    info!("Read properties file");
-    let _property = args.property;
+    let _property_values = get_property_values(&args.property_fn, &args.property)?;
 
-    let recs = fasta_parser::parse_alignment(alignment_content);
-    info!("Got {:?} records", recs.len());
-
-    for rec in &recs {
-        println!("{}: {}", rec.name, rec.sequence.len());
-        println!("{}: {}", rec.name, rec.sequence);
+    for sequence in &sequences
+    {
+        println!("{}: {}", sequence.name, sequence.sequence);
     }
-    println!("Read {} records",recs.len());
+    println!("Read {} records",sequences.len());
     
     Ok(())
 }
